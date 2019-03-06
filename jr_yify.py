604a805380b9ae8ec4de1cefe00e5125dfb13bc0
Justin Riddell <arghnews@hotmail.co.uk>
2019-03-06 15:52:46 +0000

    Combine into one file only

--- jr_yify.py
+++ jr_yify.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-#VERSION: 0.01
+#VERSION: 0.1
 #AUTHORS: Justin Riddell
 
 import sys
@@ -8,7 +8,159 @@ import sys
 import aiohttp
 import asyncio
 
-import pagination
+# This has been written in a different (and more liberating) style from my usual
+# stuff in the sense there is no error checking at all. The idea being that
+# failure is non-critical and relatively easy to reproduce, test and fix. This
+# makes the code faster to write and more compact.
+
+### Start of pagination.py
+
+# This was a separate file, however the instructions on making your own search
+# plugin (to me) do not make it explicit that your plugin MUST be in one file
+# only - it states that "that a qBittorrent search engine plugin is actually a
+# Python class file" however this does NOT say one and only one.
+# It turns out the importer uses some shenanigans (in nova2.py) messing about
+# with module imports and in the actual app when you try to import you just get
+# a generic error message. I wish this was made clear on the page or the error
+# message given wasn't a generic "failed to load plugin".
+# Simple solution, dump all this into one file.
+
+import asyncio
+import bs4
+import functools
+import re
+import sys
+
+import aiohttp
+
+import novaprinter
+
+# Three types of pages:
+# One page of results
+# http://www.yify-movies.net/search/mission impossible/
+# Multiple pages of results but no last
+# http://www.yify-movies.net/search/batman/
+# Multiple pages of results with so many there's a "last" and not all on this
+# page
+# http://www.yify-movies.net/search/the/
+
+def additional_urls(page):
+    soup = bs4.BeautifulSoup(page, features = "html.parser")
+    pages_div = soup.find("div", class_ = "pagination")
+
+    # Likely no results at all for this title
+    if not pages_div:
+        return []
+
+    # If pagination div is empty then we still need to yield this page
+    hrefs = pages_div.find_all("a")
+    if not hrefs:
+        return []
+
+    last = functools.reduce(max,
+            (int(s["href"].split("/")[-2]) for s in hrefs[1:]))
+
+    return ["/".join(["time", str(i), ""]) for i in range(2, last + 1)]
+
+async def get(session, url):
+    async with session.get(url) as resp:
+        return await resp.text()
+
+def page_movie_urls(page):
+    return {m for m in re.findall(r"href=\"(/movies[^\"]*)\"", page)}
+
+async def metadata_from_url(session, url, root):
+    # print("Getting")
+    metadata = {
+            "engine_url": root,
+            "desc_link": url,
+            "url": url,
+            }
+    page = await get(session, url)
+    # page = str(requests.get(url).content)
+    data = metadata_from_page(page)
+    metadata.update(data)
+    # doneso()
+    return metadata
+
+# For debugging to see how long each request is taking
+# def doneso():
+#     if not hasattr(doneso, "i"):
+#         doneso.i = 0
+#     print(doneso.i)
+#     doneso.i += 1
+
+def metadata_from_page(page):
+    metadata = {"leech": "-1",}
+    soup = bs4.BeautifulSoup(page, features = "html.parser")
+
+    e = soup.find("div", class_ = "heading")
+    e.contents[0].unwrap()
+    name = e.string
+    if name.endswith(" YIFY Movie"):
+        name = name[:-len(" YIFY Movie")]
+    metadata["name"] = name
+
+    year_start = name.rfind("(") + 1
+    year_end = name.rfind(")", year_start)
+    metadata["year"] = name[year_start: year_end]
+
+    metadata["link"] = soup.find("a", href = re.compile(r"^magnet:.*"))["href"]
+
+    for tag in soup.find("div", class_ = "available").ul.find_all("li"):
+        # Remove bold tag from first element
+        tag.contents[0].unwrap()
+        if tag.contents[0] == "Size:":
+            metadata["size"] = tag.contents[1].strip()
+        if tag.contents[0] == "Quality:":
+            metadata["quality"] = tag.contents[1].strip()
+        if tag.contents[0] == "Peers/Seeds:":
+            peers, seeds = tag.contents[1].replace(" ", "").split("/")
+            metadata["seeds"] = seeds
+    return metadata
+
+async def main_async(session, title, root):
+
+    # Get first set of pages
+    root_search_title = root + "/search/" + title + "/"
+    pages = [await get(session, root_search_title)]
+    urls = [root_search_title + url for url in additional_urls(pages[0])]
+    if urls:
+        pages += await asyncio.gather(*[get(session, url) for url in urls])
+    movie_urls = [url for page in pages for url in page_movie_urls(page)]
+    # print(len(pages))
+    # print(len(movie_urls))
+
+    # Remove bad quality or 3d versions
+    remove = [url for url in movie_urls if
+            (url.endswith("720p.html")
+                and url.replace("720p.html", "1080p.html") in movie_urls)
+            or url.endswith("3d.html")]
+    movie_urls = [root + url for url in movie_urls if url not in remove]
+    # print(len(movie_urls))
+
+    # Get actual metadata including magnet links
+    # print(movie_urls)
+    magnets = await asyncio.gather(
+            *[metadata_from_url(session, url, root) for url in movie_urls])
+    movies = sorted(magnets,
+            key = lambda movie: (int(movie["year"]), movie["name"]))
+
+    for movie in movies:
+        # print(movie["name"], "-", movie["year"])
+        novaprinter.prettyPrinter(movie)
+
+async def main_a(title, root):
+    async with aiohttp.ClientSession(
+            connector = aiohttp.TCPConnector(limit = 500)) as session:
+        await main_async(session, title, root)
+
+def search_me(title, root):
+    asyncio.get_event_loop().run_until_complete(main_a(title, root))
+
+### End of pagination.py
+
+
 
 _ = """
 Required for qbittorrent else -1
@@ -37,11 +189,11 @@ yify_root = "http://www.yify-movies.net"
 
 class jr_yify:
     url = yify_root
-    name = "jr-yify-movies.net"
+    name = "yify-movies.net"
     supported_categories = {"all": "0", "movies": "6",}
 
     def search(self, what, cat = "movies"):
-        pagination.search_me(what, jr_yify.url)
+        search_me(what, jr_yify.url)
 
 def main(argv):
     searcher = jr_yify()
